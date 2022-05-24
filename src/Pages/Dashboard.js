4265c74ddbacbe19b5bb132e6cf6c6ebff5fd2a1
mdjasimf <mdjasimf@gmail.com>
2022-05-24 11:40:50 +0600

    my ordeers and cancel button done

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -12,7 +12,7 @@ const Dashboard = () => {
             </div>
             <div class="drawer-side">
                 <label for="my-drawer-2" class="drawer-overlay"></label>
-                <ul class="menu p-4 overflow-y-auto w-80 bg-gray-500 text-base-content">
+                <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 bg-slate-100 text-base-content">
                     <li><Link to='/dashboard'>My Orders</Link></li>
                     <li><Link to='/dashboard/myprofile'>My Profile</Link></li>
                 </ul>
--- src/Pages/MyOrders.js
+++ src/Pages/MyOrders.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../firebase.init';
 
 const MyOrders = () => {
+    const [orders, setOrders] = useState([]);
+    const [user, loading] = useAuthState(auth);
+    useEffect(() => {
+        const getItems = async () => {
+            const email = user.email;
+            const url = `http://localhost:5000/orders?email=${email}`;
+            const { data } = await axios.get(url);
+            setOrders(data);
+
+        }
+        getItems();
+    }, [user])
+
+    const handleMyItemDelete = id => {
+        const permit = window.confirm('Sure want to delete');
+        if (permit) {
+            const url = `http://localhost:5000/orders/${id}`
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(response => response.json())
+                .then(data => {
+                    const remaining = orders.filter(myItem => myItem._id !== id)
+                    setOrders(remaining);
+                })
+        }
+    }
+
+
     return (
         <div>
-            <h2>this orders</h2>
+            {
+                orders.map(order => <div key={order._id}>
+                    <h1>{order.name}</h1><button class="btn btn-outline btn-primary btn-xs" onClick={() => handleMyItemDelete(order._id)}>Delete</button>
+
+                </div>)
+            }
         </div>
     );
 };
